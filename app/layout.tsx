40a4291e78ca75207fef1a42a84b916c7184f87f
Hyago <hyagosilvaxds@gmail.com>
2025-05-27 13:23:36 -0300

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Praivy',
+  description: 'Encontre as melhores criadoras de conteúdo',
 }
 
 export default function RootLayout({
